5864bfa0d631232ac46c6d981cae023c025c333d
Stewart McKee <stewart@theizone.co.uk>
2019-09-16 23:18:22 +0100

    syng stdout so logging works

--- lib/heartbeat_monitor.rb
+++ lib/heartbeat_monitor.rb
@@ -6,6 +6,8 @@ require 'heartbeat_monitor/channels/channel'
 require 'logger'
 require 'json'
 
+$stdout.sync
+
 Dir[File.dirname(__FILE__) + '/heartbeat_monitor/channels/*.rb'].each { |file| require file }
 
 LOGGER = Logger.new(STDOUT)
